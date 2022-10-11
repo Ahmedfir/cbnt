b8384516423db17de2b3dd4077f97572b8970bdb
ahmed khanfir <ahmed.khanfir.89@gmail.com>
2022-10-11 17:44:01 +0200

    added the feature to output the mutated classes

--- cb/replacement_mutants.py
+++ cb/replacement_mutants.py
@@ -1,5 +1,7 @@
 import logging
 import sys
+from os import makedirs
+from os.path import join, isfile, isdir
 from subprocess import TimeoutExpired
 from typing import List
 
@@ -23,12 +25,25 @@ class ReplacementMutant:
         self.compilable = None
         self.broken_tests = None
 
+    def apply_mutation(self, original):
+        return original[: self.start] + self.replacement + original[self.end:]
+
+    def output_mutated_file(self, output_dir, tmp_original_file=None):
+        output_file = join(output_dir, str(self.id), self.file_path)
+        if not isfile(output_file):
+            if not isdir(join(output_dir, str(self.id))):
+                makedirs(join(output_dir, str(self.id)))
+            if tmp_original_file is None:
+                tmp_original_file = load_file(self.file_path)
+            mutated_file = self.apply_mutation(tmp_original_file)
+            write_file(output_file, mutated_file)
+
     def compile_execute(self, project, tmp_original_file=None, reset=True):
         log.debug('{0} - compile_execute in {1}'.format(str(self.id), self.file_path))
 
         if tmp_original_file is None:
             tmp_original_file = load_file(self.file_path)
-        mutated_file = tmp_original_file[: self.start] + self.replacement + tmp_original_file[self.end:]
+        mutated_file = self.apply_mutation(tmp_original_file)
         try:
             write_file(self.file_path, mutated_file)
             self.compilable = project.compile()
